6e5303657304e522bea40ebcfd8bb77ad108bab7
Yoyo6K <78444206+Yoyo6K@users.noreply.github.com>
2023-01-17 21:51:07 +0100

    product cart model

---
+++ ClassLibrary/Cart.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassLibrary
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public User user { get; set; }
+        public bool isValidated { get; set; }
+        public List<Product> listOfProducts { get; set; }
+        public DateTime dateValidation { get; set; }
+    }
+}
---
+++ ClassLibrary/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace ClassLibrary
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string image { get; set; }
+        public float price { get; set; }
+        public bool available { get; set; }
+        public DateTime added_time { get; set; }
+
+    }
+}

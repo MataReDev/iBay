4792c8b1007b007aa02d1ca261e7e5aa9f7f9e44
Yoyo6K <78444206+Yoyo6K@users.noreply.github.com>
2023-01-18 17:58:45 +0100

    context et program

--- iBay/Context.cs
+++ iBay/Context.cs
@@ -13,6 +13,6 @@ namespace iBay
             : base(options) { }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlServer(@"Data Source=DESKTOP-0M2IDL4\Yoann;Initial Catalog=ibay;Integrated Security=True;Trust Server Certificate=true");
+            => options.UseSqlServer(@"Data Source=DESKTOP-0M2IDL4\sqlexpress;Initial Catalog=ibay;Integrated Security=True;Trust Server Certificate=true");
     }
 }

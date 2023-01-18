3b1f652ef3e48c2bd3fa06284af5ba1c4371a6f7
MataReDev <goudalmathys@gmail.com>
2023-01-18 21:05:42 +0100

    Add Tool folder with password method

---
+++ iBay/Tools/Password.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace iBay.Tools
+{
+    public class Password
+    {
+        public static string hashPassword(string password)
+        {
+            var sha = SHA256.Create();
+            var byteArray = Encoding.Default.GetBytes(password);
+            var hashPassword = sha.ComputeHash(byteArray);
+            return Convert.ToBase64String(hashPassword);
+        }
+
+    }
+}

3ff122f13f3836277f75df489c3874b2114248ea
Yoyo6K <78444206+Yoyo6K@users.noreply.github.com>
2023-01-21 21:28:57 +0100

    Add migrations

---
+++ iBay/Migrations/20230121182559_Update1.7.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace iBay.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update17 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductCart_Product_productId",
+                table: "ProductCart");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProductCart_productId",
+                table: "ProductCart");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductCart_productId",
+                table: "ProductCart",
+                column: "productId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductCart_Product_productId",
+                table: "ProductCart",
+                column: "productId",
+                principalTable: "Product",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

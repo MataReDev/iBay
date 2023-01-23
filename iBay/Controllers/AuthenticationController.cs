c02a039f9232eae4451919744c658cf51d18ed88
MataReDev <goudalmathys@gmail.com>
2023-01-23 23:13:31 +0100

    Add uth system

---
+++ iBay/Controllers/AuthenticationController.cs
@@ -0,0 +1,72 @@
+﻿using iBay.Tools;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace iBay.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly Context _context;
+        private readonly IConfiguration _config;
+
+        /// <summary>
+        /// Constructeur product
+        /// </summary>
+        /// <param name="context"></param>
+        public AuthenticationController(Context context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        /// <summary>
+        /// Provides login system
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <returns>Return a login token</returns>
+        /// <response code="401">Email or Password is incorrect</response>
+        /// <response code="200">Return a token</response>
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login(string email, string password)
+        {
+            String hashPassword = Password.hashPassword(password);
+
+            var dbUser = _context.User.Where(u => u.Email == email.ToLower() && u.Password == hashPassword).FirstOrDefault();
+            if (dbUser == null) return Unauthorized("Email or Password is incorrect");
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, dbUser.Email),
+            };
+
+            string token = GenerateTokenString(_config["Jwt:Key"],claims);
+            return Ok(token);
+        }
+
+        [NonAction]
+        public string GenerateTokenString(string secret, List<Claim> claims)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
+            var tokenDescription = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(60),
+                SigningCredentials = new SigningCredentials(
+                    key,
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescription);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
--- iBay/Program.cs
+++ iBay/Program.cs
@@ -1,8 +1,12 @@
-using iBay;
+using iBay.Controllers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Reflection;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
@@ -25,19 +29,23 @@ builder.Services.AddSwaggerGen(options =>
     // Use the xml in swagger
     options.IncludeXmlComments(xmlPath); 
 });
-builder.Services.AddDbContext<Context>();
-//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
-//    options.TokenValidationParameters = new TokenValidationParameters
-//    {
-//        ValidateIssuer = true,
-//        ValidateAudience = true,
-//        ValidateLifetime = true,
-//        ValidateIssuerSigningKey = true,
-//        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-//        ValidAudience = builder.Configuration["Jwt:Audience"],
-//        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-//    };
-//});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme= JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateIssuerSigningKey = false,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
 
 var app = builder.Build();
 
@@ -54,7 +62,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseAuthentication();
+app.UseAuthentication();
 
 app.UseHttpsRedirection();
 
